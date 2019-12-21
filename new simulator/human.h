a8b01f12c282db8bb5e26bb8a65589350b734652
songx97 <40930596+songx97@users.noreply.github.com>
2019-12-21 16:40:28 +0800

    implement model of human

--- new simulator/human.h
+++ new simulator/human.h
@@ -5,6 +5,14 @@
 #ifndef NEW_SIMULATOR_HUMAN_H
 #define NEW_SIMULATOR_HUMAN_H
 
+class way
+{
+  public:
+  int x, y;
+  way * prev;
+  way(int x_, int y_, way* prev_) :x(x_), y(y_), prev(prev_) {}
+  way() {}
+};
 
 class human {
 public:
@@ -13,6 +21,12 @@ public:
     void get_next(int *x, int *y, const int game_map[][150]);
 private:
     int x, y;
+    int goalX, goalY;
+    int last, now;
+    way next[22600];
+    int stepNow;
+    void next_dst(int *x, int *y);
+    void path_search(const int maze[][150]);
 };
 
 
